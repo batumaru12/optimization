d58d76a3367c3cb5e8576006107265e39abab2cd
batumaru12 <taku02141212@gmail.com>
2025-08-27 02:43:50 +0900

    2025-08-25

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -4,18 +4,21 @@ from skopt import Optimizer
 from skopt.space import Real
 import glob
 
-file_name = glob.glob("./data/*result.csv")[0]
+data_file_name = glob.glob("./data/*result.csv")[0]
+maxmin_file_name = glob.glob("./data/*maxmin.csv")[0]
 
-df = pd.read_csv(file_name)
+df = pd.read_csv(data_file_name)
+maxmin = pd.read_csv(maxmin_file_name)
 target_col = "yield"
 
 X = df.drop(["yield"], axis=1)
 Y_score = df["yield"].values
 
-Y = -Y_score  
+Y = -Y_score
 
 space = []
-
+for i in range(len(X)):
+    space.append(Real(maxmin.iloc[0, i], maxmin.iloc[1, i], name=X.columns.values[i]))
 
 opt = Optimizer(
     dimensions=space,
