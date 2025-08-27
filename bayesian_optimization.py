4a1ee4782d1ede23440aa6844a956a538f1e70e0
batumaru12 <taku02141212@gmail.com>
2025-08-27 10:31:44 +0900

    2025-08-25

--- bayesian_optimization.py
+++ bayesian_optimization.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import numpy as np
 from skopt import Optimizer
 from skopt.space import Real
 import glob
 
 data_file_name = glob.glob("./data/*optimization.csv")[0]
-maxmin_file_name = glob.glob("./data/*maxmin.csv")[0]
+maxmin_file_name = glob.glob("./data/*setup.csv")[0]
 
 df = pd.read_csv(data_file_name)
 maxmin = pd.read_csv(maxmin_file_name)
@@ -16,7 +17,7 @@ Y_score = df["yield"].values
 Y = -Y_score
 
 space = []
-for i in range(len(X)):
+for i in range(X.shape[1]):
     space.append(Real(maxmin.iloc[0, i], maxmin.iloc[1, i], name=X.columns.values[i]))
 
 opt = Optimizer(
@@ -32,3 +33,10 @@ next_X = opt.ask()
 
 suggestion = {name: round(val, 4) for name, val in zip(X.columns.values, next_X)}
 print(suggestion)
+
+append_row = {**suggestion, target_col: np.nan}
+append_df = pd.DataFrame([[append_row.get(c, np.nan) for c in df.columns]], columns=df.columns)
+
+append_df.to_csv(data_file_name, mode="a", header=False, index=False, float_format="%.4f")
+
+print(f"最適化結果を{data_file_name}に追記しました")
